c0aff6744b957ad7c26f58250dc6702cf5f056cb
Edgss <eduardoogomess211@gmail.com>
2025-06-27 19:32:37 -0300

    Atualização 1

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function bemVindo(){
     const audio = document.getElementById("audio")
+    let inicio = document.getElementById("inicio")
 
+
+    inicio.classList.add("sobe")
     audio.play()
+    
+    setTimeout(function(){
+        inicio.style.display = "none"
+    }, 1600)
 }
